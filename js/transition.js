720b229c805af5fd37033b65fc251c8b7e2392df
rhyostudios <accounts@rhyostudios.com>
2022-07-25 18:30:32 +0530

    V1.1

--- js/transition.js
+++ js/transition.js
@@ -6,27 +6,6 @@ const toPage = (pageNumber) => {
 
     wrapper.style.transform = `translateX(-${leftValue}vw)`
 
-    // switch (pageNumber) {
-    //     case 0:
-    //         wrapper.style.transform = 'translateX(0vw)'
-    //         break;
-    //     case 1:
-    //         wrapper.style.transform = 'translateX(-100vw)'
-    //         break;
-    //     case 2:
-    //         wrapper.style.transform = 'translateX(-200vw)'
-    //         break;
-    //     case 3:
-    //         wrapper.style.transform = 'translateX(-300vw)'
-    //         break;
-    //     case 4:
-    //         wrapper.style.transform = 'translateX(-400vw)'
-    //         break;
-    
-    //     default:
-    //         break;
-    // }
-
     document.getElementById("burger-line-1").className="burger-menu-lines burger-menu-line-1"
     document.getElementById("burger-line-2").className="burger-menu-lines burger-menu-line-2"
     document.getElementById("burger-line-3").className="burger-menu-lines burger-menu-line-3"
